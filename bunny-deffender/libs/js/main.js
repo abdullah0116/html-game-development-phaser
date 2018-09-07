7ba342782c2b0b3bf28f1da8f0c4e9beefd01f37
Abdullah Kareem <akasak56@gmail.com>
2018-09-06 23:33:01 -0600

    fixed bugs.

--- bunny-deffender/libs/js/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-  var game = new Phaser.Game(540, 960, Phaser.AUTO, 'gameContainer');
-  game.state.add('Boot', bunnyDefender.Boot);
-  game.state.add('Preloader', bunnyDefender.Preloader);
-  game.state.start('Boot');
-}
